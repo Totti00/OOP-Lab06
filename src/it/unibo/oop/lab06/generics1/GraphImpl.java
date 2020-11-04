1decc40a1e4cea58437bbe89bd9de3c981704cca
Giacomo <giacomo.totaro2@studio.unibo.it>
2020-11-04 19:17:45 +0100

    Iniziato graph

---
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -0,0 +1,33 @@
+package it.unibo.oop.lab06.generics1;
+
+import java.util.List;
+import java.util.Set;
+
+public class GraphImpl implements Graph{
+
+	public void addNode(Object node) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEdge(Object source, Object target) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public Set nodeSet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Set linkedNodes(Object node) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public List getPath(Object source, Object target) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
